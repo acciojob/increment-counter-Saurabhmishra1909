322bf954e695312b46cba1f7cd1d13450f88ab97
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-20 00:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let counter=0;
-const counterElement=document.getElementById('counter');
-const incrementBtn=document.getElementById('incrementBtn');
-incrementBtn.addEventListener('click',()=>{
-	alert(`${counter}`);
-	counter++;
-	counterElement.textContent=counter;
+let counter = 0;
+const counterElement = document.getElementById('counter');
+const incrementBtn = document.getElementById('incrementBtn');
+
+incrementBtn.addEventListener('click', () => {
+  alert(counter); // Show the current counter value as a number
+  counter++; // Increment the counter
+  counterElement.textContent = counter; // Update the counter display
 });
