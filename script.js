9a1f6e94817ecfaf4d3c74574e3c4215a94ddfba
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-20 00:04:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let counter=0;
 const counterElement=document.getElementById('counter');
 const incrementBtn=document.getElementById('incrementBtn');
-clk.addEventListener('click',()=>{
+incrementBtn.addEventListener('click',()=>{
 	alert(`Un-incremented value: ${counter}`);
 	counter++;
 	counterElement.textContent=counter;
